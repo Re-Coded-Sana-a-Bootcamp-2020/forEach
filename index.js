6013a11d5cd9c50f1273bbb8b6a34dc6c4be7070
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-24 13:11:33 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Defin an array with min of 3 name in small letters
+const names = ['muhanned','maher','salah'];
+// This array will contain your names after turning them into upper case.
+let uppercaseNames = [];
+
+// With a for loop you might do something like this.
+// for (let i = 0; i < names.length; i++) {
+//   uppercaseNames.push(names[i].toUpperCase());
+// }
+
+
+// Do the same thing the upper function does using forEach function.
+
+console.log(uppercaseNames)
